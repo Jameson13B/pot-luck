810d8add0630ecac965040033581fea0a40a9cd7
Jameson Brown <jamesonbrown@neolivity.com>
2019-07-26 21:12:45 -0600

    Added login and input change functionality

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,27 +1,37 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { auth } from '../Firebase'
 
 class Login extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      username: "",
-      passowrd: ""
-    };
+      email: '',
+      password: '',
+      error: null
+    }
   }
+
+  handleInputChange = e => this.setState({ [e.target.name]: e.target.value })
+  handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .catch(err => this.setState({ error: err.message }))
+  }
+
   render() {
     return (
       <LoginContainer>
         <h1>Login</h1>
         {/* Remove h1 above and create login form here with HTML */}
       </LoginContainer>
-    );
+    )
   }
 }
 
-export default Login;
+export default Login
 
 // Create CSS here:
 const LoginContainer = styled.div`
   max-width: 550px;
-`;
+`
